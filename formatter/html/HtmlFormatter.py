135bfa37ea43ffd97dd093b9cafc3c628f97c672
Vsevolod Djagilev <redcode.sys@gmail.com>
2016-11-17 11:57:58 +0200

    Print initial meta data

--- formatter/html/HtmlFormatter.py
+++ formatter/html/HtmlFormatter.py
@@ -2,20 +2,33 @@ from formatter.AbstractFormatter import AbstractFormatter
 import json
 import os
 
+
 class HtmlFormatter(AbstractFormatter):
     def make_file(self):
-        rf = open(self.result_file, 'r')
+        rf = open(self.result_file, 'r', encoding='utf-8')
 
         self.result_data = json.load(rf)
         rf.close()
 
-        of = open(self.output_file, 'w')
+        of = open(self.output_file, 'w', encoding="utf-8")
 
         of.write('<!DOCTYPE html><html><head>')
         of.write('<title>%s</title>' % os.path.basename(self.result_file))
         of.write('</head><body>')
 
-        
+        # Main data
+        of.write("<table>")
+
+        of.write("<tr><td><b>Author:</b></td><td>%s</td></tr>" % self.result_data['author'])
+        of.write("<tr><td><b>Config file:</b></td><td>%s</td></tr>" % self.result_data['config']['file'])
+        of.write("<tr><td><b>Config file SHA256:</b></td><td>%s</td></tr>" % self.result_data['config']['sha256'])
+        of.write("<tr><td><b>Evidence folder path:</b></td><td>%s</td></tr>" % self.result_data['evidence_folder'])
+
+        of.write("</table>")
+
+        of.write("<hr>")
+
+        of.write("<h1>Result</h1>")
 
         of.write('</body></html>')
         of.close()
